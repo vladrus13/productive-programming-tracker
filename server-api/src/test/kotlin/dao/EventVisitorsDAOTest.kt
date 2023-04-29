00f716adecf394d97df4a3d43c4b75df29a04fea
tarasska <taras.skazhenik@yandex.ru>
2023-04-29 15:47:34 +0300

    Added EventVisitorsDAOTest

--- server-api/src/test/kotlin/dao/EventVisitorsDAOTest.kt
+++ server-api/src/test/kotlin/dao/EventVisitorsDAOTest.kt
@@ -6,6 +6,7 @@ import model.EventVisitor.VisitStatus
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.update
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -75,4 +76,67 @@ class EventVisitorsDAOTest: AbstractEventTest("test_db", arrayOf(Events, EventVi
             assertEquals(VisitStatus.R, actualEventVisitor.visitStatus)
         }
     }
+
+    @Test
+    fun testUpdateExistingEventVisitor() {
+        runBlocking {
+            defaultEventVisitorsTableSetup()
+
+            val initialEventVisitor1 = dao.findById(1L)
+            assertNotNull(initialEventVisitor1)
+            val newEventVisitor1 = EventVisitor(1L, initialEventVisitor1.eventId, initialEventVisitor1.fullName, VisitStatus.V)
+
+            val savedEventVisitorId = dao.upsert(newEventVisitor1)
+            assertNotNull(savedEventVisitorId)
+            assertEquals(1L, savedEventVisitorId)
+
+            val actualEventVisitor1 = dao.findById(1L)
+            assertNotNull(actualEventVisitor1)
+            assertEquals(initialEventVisitor1.id, actualEventVisitor1.id)
+            assertEquals(initialEventVisitor1.eventId, actualEventVisitor1.eventId)
+            assertEquals(VisitStatus.V, actualEventVisitor1.visitStatus)
+        }
+    }
+
+    @Test
+    fun testUpdateNonExistingEventVisitor() {
+        runBlocking {
+            defaultEventVisitorsTableSetup()
+
+            val eventVisitorIdNotExist: Long = 12345
+            val savedEventId = dao.upsert(EventVisitor(eventVisitorIdNotExist, 1L, "II", VisitStatus.M))
+            assertNull(savedEventId)
+
+            val actualEvent = dao.findById(eventVisitorIdNotExist)
+            assertNull(actualEvent)
+        }
+    }
+
+    @Test
+    fun testDelete() {
+        runBlocking {
+            defaultEventVisitorsTableSetup()
+
+            val initialEventVisitor3 = dao.findById(3)
+            assertNotNull(initialEventVisitor3)
+
+            assertTrue(dao.delete(3))
+            assertNull(dao.findById(3))
+            assertFalse(dao.delete(3))
+        }
+    }
+
+    @Test
+    fun testFindAllByEventId() {
+        runBlocking {
+            defaultEventVisitorsTableSetup(3 * defaultEventCnt) // 3 visitor per event
+
+            val eventId = 1L
+            val visitors = dao.findAllByEventId(eventId)
+
+            assertEquals(3, visitors.size)
+            assertTrue(visitors.all { it.eventId == eventId })
+        }
+    }
+
 }
