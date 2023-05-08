66734e5b1a0ef002fef2ddd5af36628359d9603b
vladislav.kuznetsov <vladrus13rus@yandex.ru>
2023-05-08 15:10:44 +0400

    [server-bot]. Add logging to messages

--- server-bot/src/main/kotlin/ru/productive/bot/commands/EventCmd.kt
+++ server-bot/src/main/kotlin/ru/productive/bot/commands/EventCmd.kt
@@ -6,16 +6,24 @@ import com.github.kotlintelegrambot.entities.ChatId
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
 import ru.productive.api.client.ApiClient
+import ru.productive.bot.botLogger
 import ru.productive.bot.commands.parser.parseAddEventTitle
+import ru.productive.utils.LoggerUtils.Companion.addAnswer
+import ru.productive.utils.LoggerUtils.Companion.addFailAnswer
+import ru.productive.utils.LoggerUtils.Companion.addUserMessage
 
 fun Dispatcher.addEvent(apiClient: ApiClient) {
     command("addEvent") {
         runBlocking {
+            botLogger.addUserMessage("addEvent", message)
             parseAddEventTitle(message.text)
                 .onSuccess { title ->
                     val response: HttpResponse = apiClient.addEvent(title)
-                    bot.sendMessage(ChatId.fromId(message.chat.id), text = response.bodyAsText())
+                    val textResponse = response.bodyAsText()
+                    botLogger.addAnswer("addEvent", message, textResponse)
+                    bot.sendMessage(ChatId.fromId(message.chat.id), text = textResponse)
                 }.onFailure { e ->
+                    botLogger.addFailAnswer("addEvent", message, e.stackTrace.toString())
                     bot.sendMessage(ChatId.fromId(message.chat.id), text = e.message ?: "Error")
                 }
         }
--- server-bot/src/main/kotlin/ru/productive/bot/commands/EventVisitorsCmd.kt
+++ server-bot/src/main/kotlin/ru/productive/bot/commands/EventVisitorsCmd.kt
@@ -6,18 +6,26 @@ import com.github.kotlintelegrambot.entities.ChatId
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
 import ru.productive.api.client.ApiClient
+import ru.productive.bot.botLogger
 import ru.productive.bot.commands.parser.parseAddEventVisitorArguments
 import ru.productive.bot.commands.parser.parseGetEventVisitorEventId
+import ru.productive.utils.LoggerUtils.Companion.addAnswer
+import ru.productive.utils.LoggerUtils.Companion.addFailAnswer
+import ru.productive.utils.LoggerUtils.Companion.addUserMessage
 
 fun Dispatcher.getEventVisitors(apiClient: ApiClient) {
     command("getEventVisitors") {
         runBlocking {
+            botLogger.addUserMessage("getEventVisitors", message)
             parseGetEventVisitorEventId(message.text)
                 .onSuccess { eventId ->
                     val response: HttpResponse = apiClient.getVisitors(eventId)
-                    bot.sendMessage(ChatId.fromId(message.chat.id), text = response.bodyAsText())
+                    val textResponse = response.bodyAsText()
+                    botLogger.fine("Answer to ${message.from?.id}: $textResponse")
+                    bot.sendMessage(ChatId.fromId(message.chat.id), text = textResponse)
                 }
                 .onFailure { e ->
+                    botLogger.addFailAnswer("addEvent", message, e.stackTrace.toString())
                     bot.sendMessage(ChatId.fromId(message.chat.id), text = e.message ?: "Error")
                 }
         }
@@ -28,12 +36,16 @@ fun Dispatcher.addEventVisitor(apiClient: ApiClient) {
 
     command("addEventVisitor") {
         runBlocking {
+            botLogger.addUserMessage("addEventVisitors", message)
             parseAddEventVisitorArguments(message.text)
                 .onSuccess { (eventId, fullName) ->
                     val response: HttpResponse = apiClient.addVisitor(eventId, fullName)
-                    bot.sendMessage(ChatId.fromId(message.chat.id), text = response.bodyAsText())
+                    val textResponse = response.bodyAsText()
+                    botLogger.addAnswer("addEventVisitors", message, textResponse)
+                    bot.sendMessage(ChatId.fromId(message.chat.id), text = textResponse)
                 }
                 .onFailure { e ->
+                    botLogger.addFailAnswer("addEventVisitors", message, e.stackTrace.toString())
                     bot.sendMessage(ChatId.fromId(message.chat.id), text = e.message ?: "Error")
                 }
         }
